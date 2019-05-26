7827b30d4753473917c40701e465bb570d45f419
Deevy Bhimani <deevy.bhimani@berkeley.edu>
2019-05-25 23:02:45 -0700

    iPad landscape compatibility

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,8 +3,6 @@ $(document).ready(function(){
 	// Typing Animation
 	new TypeIt('#whatIAm', {
 		strings: [' a data enthusiast.', ' an aspiring product manager.', ' a minimalist.', ' a motorcycle enthusiast.', ' driven.'],
-		speed: 200,
-		deleteSpeed: 60,
 		breakLines: false,
 		nextStringDelay: 2000,
 		startDelay: 550
