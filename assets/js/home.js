74251caebf37a731edb4d6023df7bab5172d9594
Deevy Bhimani <deevy.bhimani@berkeley.edu>
2020-12-10 23:38:16 -0800

    rdv updates

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	// Typing Animation
 	new TypeIt('#whatIAm', {
-		strings: [' an aspiring product manager.', ' a minimalist.', ' a motorcycle enthusiast.', ' driven.'],
+		strings: [' an aspiring product manager.', ' a motorcycist.', ' a Formula 1 enthusiast.', ' driven.'],
 		breakLines: false,
 		nextStringDelay: 2000,
 		startDelay: 550
